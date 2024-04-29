d0610808b827d4b03b5db4577c3f5c23b8ac2491
Nick <nicholasgalante@hotmail.co.uk>
2024-04-29 13:22:26 -0300

    problem 2634 solved

--- 2634.js
+++ 2634.js
@@ -5,7 +5,8 @@ The fn function takes one or two arguments:
 
 arr[i] - number from the arr
 i - index of arr[i]
-filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) 
+returns true.
 
 Please solve it without the built-in Array.filter method.
 */
@@ -15,8 +16,8 @@ Explanation:
 const newArray = filter(arr, fn); // [20, 30]
 The function filters out values that are not greater than 10
 */
-/* let arr = [0,10,20,30]; */
-function greaterThan10(n) {return n > 10; }
+let arr = [0, 10, 20, 30];
+function greaterThan10(n) { return n > 10; }
 
 /* Example 2 - Output: [1] 
 Explanation:
@@ -30,10 +31,18 @@ function firstIndex(n, i) { return i === 0; }
 Explanation:
 Falsey values such as 0 should be filtered out
 */
-let arr = [-2, -1, 0, 1, 2];
-function plusOne(n) {return n + 1}
+/* let arr = [-2, -1, 0, 1, 2]; */
+function plusOne(n) { return n + 1 }
 
 
-var filter = function(arr, fn){
+var filter = function (arr, fn) {
+    const newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            newArray.push(arr[i])
+        }
+    }
+    return newArray;
+}
 
-};
+console.log(filter(arr, greaterThan10));
