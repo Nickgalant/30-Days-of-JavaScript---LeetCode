a43313538c926c1897854032621200863418d015
Nick <nicholasgalante@hotmail.co.uk>
2024-05-14 07:06:43 -0300

    problem 2629 solved

--- 2629.js
+++ 2629.js
@@ -8,11 +8,13 @@ The function composition of an empty list of functions is the identity function 
 You may assume each function in the array accepts one integer as input and returns one integer as output. */
 
 var compose = function (functions) {
-
     return function (x) {
+       
+    };
+}
 
-    }
-};
+const fn = compose([x => x + 1, x => 2 * x]);
+console.log(fn(4));
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
