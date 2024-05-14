de83c10c7de6568b8237dc593506b2ee0b48b79c
Nick <nicholasgalante@hotmail.co.uk>
2024-05-14 12:48:04 -0300

    problem 2629 solved

--- 2629.js
+++ 2629.js
@@ -9,7 +9,11 @@ You may assume each function in the array accepts one integer as input and retur
 
 var compose = function (functions) {
     return function (x) {
-       
+        let result = x;
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
+        return result;
     };
 }
 
